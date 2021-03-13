dcb8cfc275b7d96b4bd5609c5f35b6519bc0d7bb
parin23 <parinjasoliya@gmail.com>
2021-03-13 21:38:06 +0530

    A working version of the bot

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 import wa from '@open-wa/wa-automate';
 import Scraper from 'images-scraper';
+import Promise from 'promise';
 
 wa.create().then(client => start(client));
 
 const google = new Scraper({
   puppeteer: {
-    headless: true,
+    headless: true,// Set this as False for not using Headless
   },
 });
 
+
 function start(client) {
   client.onMessage(async message => {
-	//console.log(message.body);
-	//client.sendText(message.from,"Hello");
-	//if(message.body[0]=="#"){
-		
-		//term[0]=' ';
-		await (async () => {
-  		 const results = await google.scrape(term, 100);
- 		// console.log(results[1].url);
-			var ind=await Math.floor(Math.random() * 90);
-			//const lin = results[ind];
-			console.log(results[ind]);
-			await client.sendFileFromUrl(message.from, results[ind].url,'','',message.id);
+	if(message.body.startsWith('!')){
 
-		})();
-  	//}
+		let myPromise = new Promise(function(myResolve, myReject) {
+			(async () => {
+ 			 var results = 0
+ 			 results = await google.scrape(message.body, 100);
+  			//console.log('results', results);
+  			if (results != 0) {
+ 			   myResolve(results);
+  			} else {
+ 			   myReject("Error");
+ 			 }
+			})();
+		
+		});
+  		
+  		myPromise.then(
+ 		 	 function(value) {
+ 		 	console.log('URL fetched');
+			const ind= Math.floor(Math.random() * 90);
+			//const lin = results[ind];
+		//	console.log(results)
+		//	console.log(value);
+			 client.sendFileFromUrl(message.from, value[ind].url,'','',message.id);
+			console.log('Image Sent');
+ 		 	 },
+			  function(error) {
+			  console.log(error);
+			   client.sendText(message.from, 'Please SLOW Down,');
+			  }
+			);
+ 		
+		
+  	}
   });
 }
